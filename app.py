f7d720c8ecfb20d418394b111dad1827c755b1a5
ronni86bit <cherukurirohith005@gmail.com>
2025-04-16 07:56:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request
+from language_tool_python import LanguageTool
 
+app = Flask(__name__)
+tool = LanguageTool('en-US')
+
+@app.route('/')
+def index():
+    return render_template('index.html', corrected_text='')
+
+@app.route('/spell', methods=['POST'])
+def spell_check():
+    text = request.form['text']
+    errors = tool.check(text)
+    corrected_text = tool.correct(text)
+    return render_template('index.html', corrected_text=corrected_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
